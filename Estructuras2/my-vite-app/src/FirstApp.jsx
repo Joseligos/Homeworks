bc74c1e40cc4462deb43238e86367ff28ed76fed
Joseligos <133302021+Joseligos@users.noreply.github.com>
2025-02-12 18:45:41 -0500

    Challenge 2

---
+++ Estructuras2/my-vite-app/src/FirstApp.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+const title = "Este es otro componente!"
+const FirstApp = () =>{
+    return (
+        <>
+            <h1>{title}</h1>
+            <span>10</span>
+        </>
+    )
+}
+export default FirstApp
