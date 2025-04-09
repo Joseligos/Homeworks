50b4c5ca116bb29935b353e58663c7c089190cc7
Joseligos <133302021+Joseligos@users.noreply.github.com>
2025-04-09 02:09:26 -0500

    Challenge 12

--- Firebase-Prueba/src/App.jsx
+++ Firebase-Prueba/src/App.jsx
@@ -4,15 +4,17 @@ import Registro from './Registro'
 import Login from './Login'
 import { useSelector } from 'react-redux'
 import Home from './Home'
+import { Crud } from './Crud'
 
 function App() {
   const { stats } = useSelector((state) => state.auth);
   return (
     <Routes>
-      <Route path="/" element={<Registro/>}></Route>
+      <Route path="/Registro" element={<Registro/>}></Route>
       <Route path="/Login" element={<Login/>}></Route>
       <Route path="/Home" element={
         stats === "authenticated" ? <Home/> : <Navigate to="/Login"/>}></Route>
+      <Route path="/" element={<Crud/>}></Route>
     </Routes>
   )
 }
